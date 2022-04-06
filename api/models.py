222a9ea8160956b68a3cb47272ca1dd9a769627b
AbdullahHammudha <a.m.a.h97126@gmail.com>
2022-04-06 14:29:06 +0200

    initial Commit.

---
+++ api/models.py
@@ -0,0 +1,39 @@
+from random import choices
+from tkinter import CASCADE
+from django.db import models
+from django.conf import settings
+
+class StatusChoices(models.TextChoices):
+    ACTIVE = 'active'
+    INACTIVE = 'inactive'
+    DELETED = 'deleted'
+
+class doctor(models.Model):
+    name = models.CharField(max_length=256, blank=False)
+    ssn = models.CharField(max_length=64, blank=False)
+    status = models.CharField(max_length=16, choices=StatusChoices.choices, default= StatusChoices.ACTIVE)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+    updated_at = models.DateTimeField(auto_now= True, null=False)
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='doctors')
+
+class Clinic(models.Model):
+    name = models.CharField(max_length=256, blank=False)
+    ssn = models.CharField(max_length=64, blank=False)
+    status = models.CharField(max_length=16, choices=StatusChoices.choices, default=StatusChoices.ACTIVE)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+    updated_at = models.DateTimeField(auto_now=True, null=False)
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='clinics')
+
+class Contact(models.Model):
+    key = models.CharField(max_length=256)
+    value = models.CharField(max_length=256)
+    status = models.CharField(max_length=16, choices=StatusChoices.choices, default=StatusChoices.ACTIVE)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+    updated_at = models.DateTimeField(auto_now=True, null=False)
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='contacts')
+
+
+
